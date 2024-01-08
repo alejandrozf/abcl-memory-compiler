4efed096e36453ed6e037d99e3c952bd331d0754
Alejandro <alejandrozf@MacBook-Pro.fibertel.com.ar>
2024-01-08 17:37:30 -0300

    Solved the issue with the classpath! GD!!!

--- abcl-memory-compiler.lisp
+++ abcl-memory-compiler.lisp
@@ -3,6 +3,10 @@
 (in-package #:abcl-memory-compiler)
 
 
+(defun get-classpath-jars ()
+  (cdar (java:dump-classpath (java:get-current-classloader))))
+
+
 (defun compile-to-class (class-name class-source-code)
   (let ((string-builder (java:jnew "java.lang.StringBuilder"))
         (memory-compiler (java:jstatic "newInstance" "org.mdkt.compiler.InMemoryJavaCompiler"))
@@ -15,31 +19,8 @@
     (java:jcall "useOptions" memory-compiler
                   (java:jarray-from-list
                    (list "-classpath"
-                         "/Users/alejandrozf/.m2/repository/org/java-websocket/Java-WebSocket/1.5.4/Java-WebSocket-1.5.4.jar"
+                         (format nil "~{~a~^: ~}" (get-classpath-jars))
                          "-Xlint:none")))
-    ;; TODO: see (java:dump-classpath (java:get-current-classloader))
-
-    ;; (cl-user::break-args memory-compiler)
-
-
-    #+nil(let ((sb (java:jnew "java.lang.StringBuilder"))
-          (resources (java:jcall "getResources" current-classloader "/"))
-          (path-separator (java:jfield "java.io.File" "pathSeparator")))
-      (loop :while (java:jcall "hasMoreElements" resources)
-            :do (java:jcall "append" sb
-                            (java:jcall "getFile" (java:jcall "nextElement" resources)))
-                (java:jcall "append" sb
-                            path-separator))
-
-      ;; (cl-user::break-args sb)
-
-
-      (java:jcall "useOptions" memory-compiler
-                  (java:jarray-from-list
-                   (list "-classpath" "/Users/alejandrozf/.m2/*"))))
-
-    ;; (cl-user::break-args memory-compiler)
-
     (java:jcall
      "compile"
      memory-compiler
