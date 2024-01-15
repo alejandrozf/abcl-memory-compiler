5d63bee6be0c419a610bb6e791e791f10437c7d0
Alejandro Zamora <ale2014.zamora@gmail.com>
2024-01-15 00:00:20 -0300

    refactor to remove code duplication thanks to macros! GD!!!

--- abcl-memory-compiler.lisp
+++ abcl-memory-compiler.lisp
@@ -7,70 +7,54 @@
   (cdar (java:dump-classpath current-classloader)))
 
 
+(defmacro with-dynamic-compiler
+    (body
+     &key classloader extra-classpaths)
+  `(let ((memory-compiler (java:jstatic "newInstance" "org.mdkt.compiler.InMemoryJavaCompiler")))
+
+     (loop :for classpath :in ,extra-classpaths
+           :do (java:jcall "addURL" ,classloader classpath))
+
+     (java:jcall "useParentClassLoader" memory-compiler ,classloader)
+
+     (let ((classpath-jars (get-classpath-jars ,classloader)))
+       (java:jcall "useOptions" memory-compiler
+                   (java:jarray-from-list
+                    (append
+                     (if classpath-jars
+                         (list "-classpath"
+                               (format nil "~{~a~^: ~}" classpath-jars)))
+                     (list "-Xlint:none")))))
+     (progn ,@body)))
+
+
 (defun compile-to-class (class-name class-source-code
                          &key
-                           (classloader
-                            (java:jnew "org.armedbear.lisp.JavaClassLoader"
-                                       (java:jcall "getClassLoader"
-                                                   (java:jclass "org.armedbear.lisp.LispObject"))))
+                           (classloader (java:jnew "org.armedbear.lisp.JavaClassLoader"))
                            extra-classpaths)
-  (let ((string-builder (java:jnew "java.lang.StringBuilder"))
-        (memory-compiler (java:jstatic "newInstance" "org.mdkt.compiler.InMemoryJavaCompiler"))
-        (current-classloader classloader))
-
-    (java:jcall "append" string-builder class-source-code)
-
-    (loop :for classpath :in extra-classpaths
-          :do (java:jcall "addURL" current-classloader classpath))
-
-    (java:jcall "useParentClassLoader" memory-compiler current-classloader)
-
-    (let ((classpath-jars (get-classpath-jars current-classloader)))
-      (java:jcall "useOptions" memory-compiler
-                  (java:jarray-from-list
-                   (append
-                    (if classpath-jars
-                        (list "-classpath"
-                              (format nil "~{~a~^: ~}" classpath-jars)))
-                    (list "-Xlint:none")))))
-    (java:jcall
+  (with-dynamic-compiler
+    ((java:jcall
      "compile"
      memory-compiler
      class-name
-     (java:jcall "toString" string-builder))))
+     class-source-code))
+    :classloader classloader
+    :extra-classpaths extra-classpaths))
 
 
-(defun compile-to-multiple-classes
-    (classes-code-pairs
-     &key
-       (classloader
-        (java:jnew "org.armedbear.lisp.JavaClassLoader"
-                   (java:jcall "getClassLoader"
-                               (java:jclass "org.armedbear.lisp.LispObject"))))
-       extra-classpaths)
-  (let ((memory-compiler (java:jstatic "newInstance" "org.mdkt.compiler.InMemoryJavaCompiler"))
-        (current-classloader classloader))
+(defun compile-to-multiple-classes (classes-code-pairs
+                                    &key
+                                      (classloader (java:jnew "org.armedbear.lisp.JavaClassLoader"))
+                                      extra-classpaths)
+  (with-dynamic-compiler
+    ((loop :for (class-name class-source-code) :in classes-code-pairs
+           :do (java:jcall "addSource" memory-compiler class-name class-source-code))
 
-    (loop :for classpath :in extra-classpaths
-          :do (java:jcall "addURL" current-classloader classpath))
-
-    (java:jcall "useParentClassLoader" memory-compiler current-classloader)
-
-    (let ((classpath-jars (get-classpath-jars current-classloader)))
-      (java:jcall "useOptions" memory-compiler
-                  (java:jarray-from-list
-                   (append
-                    (if classpath-jars
-                        (list "-classpath"
-                              (format nil "~{~a~^: ~}" classpath-jars)))
-                    (list "-Xlint:none")))))
-
-    (loop :for (class-name class-source-code) :in classes-code-pairs
-          :do (java:jcall "addSource" memory-compiler class-name class-source-code))
-
-    (java:jcall
-     "compileAll"
-     memory-compiler)))
+     (java:jcall
+      "compileAll"
+      memory-compiler))
+    :classloader classloader
+    :extra-classpaths extra-classpaths))
 
 
 (defun quick-compile-to-class (class-name source)
