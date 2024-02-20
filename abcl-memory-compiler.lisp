94f4fa27ae945a33b1740bbf63f82849374f980f
Alejandro Zamora <ale2014.zamora@gmail.com>
2024-02-20 14:46:17 -0300

    - improving the code - updates to readme

--- abcl-memory-compiler.lisp
+++ abcl-memory-compiler.lisp
@@ -28,20 +28,6 @@
      (progn ,@body)))
 
 
-(defun compile-to-class (class-name class-source-code
-                         &key
-                           (classloader (java:jnew "org.armedbear.lisp.JavaClassLoader"))
-                           extra-classpaths)
-  (with-dynamic-compiler
-    ((java:jcall
-     "compile"
-     memory-compiler
-     class-name
-     class-source-code))
-    :classloader classloader
-    :extra-classpaths extra-classpaths))
-
-
 (defun compile-to-multiple-classes (classes-code-pairs
                                     &key
                                       (classloader (java:jnew "org.armedbear.lisp.JavaClassLoader"))
@@ -57,32 +43,58 @@
     :extra-classpaths extra-classpaths))
 
 
-(defun quick-compile-to-class (class-name source)
-  (let ((classpath (uiop:split-string
-                    (java:jstatic
-                     "getProperty"
-                     "java.lang.System"
-                     "java.class.path"))))
-    (compile-to-class
-     class-name
-     source
-     :classloader (java:get-current-classloader)
-     :extra-classpaths
-     (loop :for cp :in classpath
-           :collect (unless (equal cp "")
-                      (java:jcall "toURL" (java:jcall "toURI" (java:jnew "java.io.File" cp))))))))
+(defun compile-to-class (class-name source)
+  (labels ((compile-to-class-aux (class-name-aux class-source-code-aux
+                                  &key
+                                    (classloader (java:jnew "org.armedbear.lisp.JavaClassLoader"))
+                                    extra-classpaths)
+           (with-dynamic-compiler
+               ((java:jcall
+                 "compile"
+                 memory-compiler
+                 class-name-aux
+                 class-source-code-aux))
+             :classloader classloader
+             :extra-classpaths extra-classpaths)))
+    (let ((classpath (uiop:split-string
+                      (java:jstatic
+                       "getProperty"
+                       "java.lang.System"
+                       "java.class.path"))))
+      (compile-to-class-aux
+       class-name
+       source
+       :classloader (java:get-current-classloader)
+       :extra-classpaths
+       (loop :for cp :in classpath
+             :collect (unless (equal cp "")
+                        (java:jcall "toURL" (java:jcall "toURI" (java:jnew "java.io.File" cp)))))))))
 
 
-(defun quick-compile-to-multiple-classes (classes-code-pairs)
-  (let ((classpath (uiop:split-string
-                    (java:jstatic
-                     "getProperty"
-                     "java.lang.System"
-                     "java.class.path"))))
-    (compile-to-multiple-classes
-     classes-code-pairs
-     :classloader (java:get-current-classloader)
-     :extra-classpaths
-     (loop :for cp :in classpath
-           :collect (unless (equal cp "")
-                      (java:jcall "toURL" (java:jcall "toURI" (java:jnew "java.io.File" cp))))))))
+(defun compile-to-multiple-classes (classes-code-pairs)
+
+  (labels ((compile-to-multiple-classes-aux (classes-code-pairs
+                                             &key
+                                               (classloader (java:jnew "org.armedbear.lisp.JavaClassLoader"))
+                                               extra-classpaths)
+           (with-dynamic-compiler
+               ((loop :for (class-name class-source-code) :in classes-code-pairs
+                      :do (java:jcall "addSource" memory-compiler class-name class-source-code))
+
+                (java:jcall
+                 "compileAll"
+                 memory-compiler))
+             :classloader classloader
+             :extra-classpaths extra-classpaths)))
+    (let ((classpath (uiop:split-string
+                      (java:jstatic
+                       "getProperty"
+                       "java.lang.System"
+                       "java.class.path"))))
+      (compile-to-multiple-classes-aux
+       classes-code-pairs
+       :classloader (java:get-current-classloader)
+       :extra-classpaths
+       (loop :for cp :in classpath
+             :collect (unless (equal cp "")
+                        (java:jcall "toURL" (java:jcall "toURI" (java:jnew "java.io.File" cp)))))))))
